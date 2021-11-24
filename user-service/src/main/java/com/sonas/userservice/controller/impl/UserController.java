6b32ef9ac5251a41686924876460a2da890bc940
Anna Nodzyńska <7143565+neternefer@users.noreply.github.com>
2021-11-24 20:20:34 +0100

    Add basic CRUD methods for user

--- user-service/src/main/java/com/sonas/userservice/controller/impl/UserController.java
+++ user-service/src/main/java/com/sonas/userservice/controller/impl/UserController.java
@@ -1,13 +1,61 @@
 package com.sonas.userservice.controller.impl;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sonas.userservice.controller.dto.UserDTO;
+import com.sonas.userservice.controller.interf.IUserController;
+import com.sonas.userservice.dao.User;
+import com.sonas.userservice.repository.UserRepository;
+import com.sonas.userservice.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RequestMapping("/api/users")
-public class UserController {
+public class UserController implements IUserController {
 
-    
+    private UserService userService;
+
+    private UserRepository userRepository;
+
+    public UserController(UserService userService,
+                          UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserById(@PathVariable(name = "id") long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User with id: " + id + " not found!"));
+    }
+
+    @PutMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public User updateUser(@PathVariable(name = "id") long id,
+                                   @RequestBody UserDTO user) {
+        return userService.update(id, user);
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody @Valid UserDTO user) {
+        return userService.create(user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@PathVariable(name = "id") long id) {
+        userRepository.deleteById(id);
+    }
 }
--- user-service/src/main/java/com/sonas/userservice/service/UserService.java
+++ user-service/src/main/java/com/sonas/userservice/service/UserService.java
@@ -1,7 +1,41 @@
 package com.sonas.userservice.service;
 
+import com.sonas.userservice.controller.dto.UserDTO;
+import com.sonas.userservice.dao.User;
+import com.sonas.userservice.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User update(long id, UserDTO user) {
+        User newUser = userRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User with id: " + id + " not found!"));
+        if (user.getEmail() != null ) {
+            user.setEmail(user.getEmail());
+        }
+        if (user.getPassword() != null) {
+            user.setPassword(user.getPassword());
+        }
+        if (user.getUserType() != null) {
+            user.setUserType(user.getUserType());
+        }
+        return userRepository.save(newUser);
+    }
+
+    public User create(UserDTO user) {
+        User newUser = new User(user.getEmail(),
+                                user.getPassword(),
+                                user.getUserType()
+        );
+        return userRepository.save(newUser);
+    }
 }
