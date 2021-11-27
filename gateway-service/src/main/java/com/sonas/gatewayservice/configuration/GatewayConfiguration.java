8339f0dea4d3bfd6af887f684c89a4d15efb47dd
Anna Nodzyńska <7143565+neternefer@users.noreply.github.com>
2021-11-27 17:26:23 +0100

    Add gateway configuration

---
+++ gateway-service/src/main/java/com/sonas/gatewayservice/configuration/GatewayConfiguration.java
@@ -0,0 +1,32 @@
+package com.sonas.gatewayservice.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/api/users/**")
+                        .uri("lb://USER-SERVICE"))
+                .route(p -> p.path("/api/users**")
+                        .uri("lb://USER-SERVICE"))
+                .route(p -> p.path("/api/curriculums/**")
+                        .uri("lb://CV-SERVICE"))
+                .route(p -> p.path("/api/curriculums**")
+                        .uri("lb://CV-SERVICE"))
+                .route(p -> p.path("/api/portfolios/**")
+                        .uri("lb://PORTFOLIO-SERVICE"))
+                .route(p -> p.path("/api/portfolios**")
+                        .uri("lb://PORTFOLIO-SERVICE"))
+                .route(p -> p.path("/api/jobs/**")
+                        .uri("lb://JOB-SERVICE"))
+                .route(p -> p.path("/api/jobs**")
+                        .uri("lb://JOB-SERVICE"))
+                .build();
+    }
+}
